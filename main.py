c08e3e4642981f4a40d515e2208d1a1ca50bc5a7
arossaadhikary <148636324+arossaadhikary@users.noreply.github.com>
2024-08-04 02:02:03 -0400

    book obj creation, updates to finding targetBook
    - created book obj from book dict in data-cleaning.py - easier to understand 
    - iterating through .csv file to make book obj

--- main.py
+++ main.py
@@ -3,12 +3,34 @@ import dataCleaning
 import requests
 import pandas as pd
 
+class Book:
+    def __init__(self, isbn, title, subjects, author):
+        self.isbn = id
+        self.title = title
+        self.subjects = subjects
+        self.author = author
+
+def csvDictToObj(filePath):
+    df = pd.read_csv(filePath, low_memory=False)
+    books = []
+    for _, row in df.iterrows():
+        book = Book(
+            isbn=row['isbn'],
+            title=row['title'],
+            subjects=row['subjects'],
+            author=row['author']
+        )
+        books.append(book)
+    return books
+    
 def findTargetBook(bookObjList, targetName):
     for book in bookObjList:
         if book.Name == targetName:
             return book
 
 def main():
+    bookObjList = csvDictToObj('books_data.csv')
+    
     print("Welcome to PageTurners!")
     print("If you wish to exit your search, when prompted to enter your book title, type 'No'")
 
@@ -18,13 +40,11 @@ def main():
             break
             
         targetBook = findTargetBook(bookObjList, userInputBook)
-        print(targetBook.subjects)
-        
+        # print(targetBook.subjects)
         finalist = testing.createSimList(targetBook, bookObjList)
+        # for item in finalList: print(item)
 
     print("Thank you for using PageTurners!")
-    # similarity_score = testing.calc_sim(desc1, desc2)
-    # print(f"Similarity Score: {similarity_score}")
 
 if __name__ == "__main__":
     main()
