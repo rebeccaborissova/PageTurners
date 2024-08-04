a4cc112e143763d4cc25e558039702e40c3f2a5a
arossaadhikary <148636324+arossaadhikary@users.noreply.github.com>
2024-08-04 01:43:26 -0400

    adding calculating sim score indv. to target book + list creation

--- testing.py
+++ testing.py
@@ -5,28 +5,19 @@ from sklearn.metrics.pairwise import cosine_similarity
    TF-IDF = Term Frequency-Inverse Document Frequency --> quantify importance of words in doc.
 '''
 
-def calcCosineSimMatrix(subjDescriptions, allBookDescriptions):
-   # input = subjDescriptions = txt descriptions want to convert to TF-IDF features
+def calcSim(targetBookSubj, singleBookSubj):
+   descriptions = [targetBookSubj, singleBookSubj]
+   vectorizer = TfidfVectorizer().fit_transform(descriptions)
+   vectors = vectorizer.toarray()
+   cosine_sim = cosine_similarity(vectors)
+   similarity_score = cosine_sim[0, 1]
+   return similarity_score
 
-   # vectorizer = instance of TfidfVectorizer from sklearn.feature_extraction.text
-   # TfidfVectorizer() = converts text -> matrix
-   vectorizer = TfidfVectorizer()
-   all_descriptions = [' '.join(subjDescriptions)] + descriptions
+def createSimList(targetBook, bookObjList):
+   finalSim = []
+   for book in bookObjList:
+      if book.Name != targetBook.Name and book.subjects != []:
+         simScore = calcSim(targetBook.subjects, book.subjects)
+         finalSim.append((book.Name, simScore))
 
-   # fit = learns vocab + inverse doc freq stat from subjDescriptions
-   # transform = converts subjDescriptions -> matrix of TF-IDF features
-   # tfidf_matrix = row = doc; col = term's TF-IDF score in doc
-   tfidf_matrix = vectorizer.fit_transform(all_descriptions)
-
-   # calc cosine sim btwn vector pairs in tfidf_matrix
-   # cosine_sim_matrix = matrix (i, j) -> rep. cosine sim btwn ith & jth doc; 0-1 values
-   cosine_sim_matrix = cosine_similarity(tfidf_matrix)
-
-   return cosine_sim_matrix
-
-def buildWeightedGraph():
-   '''
-   overall idea: use the cosine_sim_matrix && create a graph based on an avg threshold calculated from np.mean(cosine_sim_matrix) in main.py
-   '''
-   pass
-
+   return finalSim
