0457aa044fd76e74d5617009b58d2ef3590164a8
Rebecca <147621579+rebeccaborissova@users.noreply.github.com>
2024-08-06 20:35:08 -0400

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -206,5 +206,9 @@ def get_book_id():
         "title": matching_book.title
     })
 
+@app.route('/test', methods=['GET'])
+def test():
+    return "Hello World"
+
 if __name__ == '__main__':
     app.run(debug=True, port=3001)
